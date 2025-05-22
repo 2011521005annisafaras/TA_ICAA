a2d39ec9a6cea99a8584766bf56a180d1847af9c
rezqyondrizal11 <67118023+rezqyondrizal11@users.noreply.github.com>
2025-05-22 21:21:41 +0700

    dashboard

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -47,23 +47,6 @@
                 </div>
             </div>
         </div>
-
-        <div class="col-xl-4 col-md-6 mb-4">
-            <div class="card border-left-success shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="h5 mb-0 font-weight-bold text-gray-800"><a href="<?= base_url('Alternatif'); ?>"
-                                    class="text-secondary text-decoration-none">Data Alternatif</a></div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-users fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
         <div class="col-xl-4 col-md-6 mb-4">
             <div class="card border-left-secondary shadow h-100 py-2">
                 <div class="card-body">
@@ -136,6 +119,37 @@
                         <div class="col mr-2">
                             <div class="h5 mb-0 font-weight-bold text-gray-800"><a href="<?= base_url('recap'); ?>"
                                     class="text-secondary text-decoration-none">Data Recap Siswa</a></div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-users fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-4 col-md-6 mb-4">
+            <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="h5 mb-0 font-weight-bold text-gray-800"><a href="<?= base_url('User'); ?>"
+                                    class="text-secondary text-decoration-none">Data User</a></div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-users fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-4 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="h5 mb-0 font-weight-bold text-gray-800"><a href="<?= base_url('Profile'); ?>"
+                                    class="text-secondary text-decoration-none">Data Profile</a></div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-users fa-2x text-gray-300"></i>
